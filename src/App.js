2f15cd5ec3a984b9f31f4216058f44c5eed5a216
Natalia Burdyey <nata.burdey@gmail.com>
2023-12-07 18:02:25 -0500

    add train table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
+import { getLineColor, getDirection } from './utils';
 import './App.css';
 
 function App() {
+  const [trainData, setTrainData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTrainPositions = async () => {
+    const url =
+      'https://api.wmata.com/TrainPositions/TrainPositions?contentType=json';
+    const headers = {
+      api_key: process.env.REACT_APP_PRIMARY_KEY,
+    };
+
+    try {
+      const response = await fetch(url, { headers });
+      const data = await response.json();
+      setTrainData(data.TrainPositions);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrainPositions();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {loading && <p>Loading...</p>}
+      <h1>WMATA Train Positions</h1>
+      {trainData && (
+        <table>
+          <thead>
+            <tr>
+              <th>Train Number</th>
+              <th>Car Count</th>
+              <th>Direction</th>
+              <th>Circuit ID</th>
+              <th>Destination Station Code</th>
+              <th>Line Code</th>
+              <th>Seconds at Location</th>
+              <th>Service Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trainData.map((train) => (
+              <tr
+                key={train.TrainId}
+                style={{ backgroundColor: getLineColor(train.LineCode) }}
+              >
+                <td>{train.TrainNumber}</td>
+                <td>{train.CarCount}</td>
+                <td>{getDirection(train.DirectionNum)}</td>
+                <td>{train.CircuitId}</td>
+                <td>{train.DestinationStationCode}</td>
+                <td>{train.LineCode}</td>
+                <td>{train.SecondsAtLocation}</td>
+                <td>{train.ServiceType}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </main>
   );
 }
 
